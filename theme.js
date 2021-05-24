d168e6751e853bdd9c80bd709ac785f97ba95b36
llamicron <llamicron@gmail.com>
2021-05-24 01:33:46 -0500

    Adds a lot more content

---
+++ theme.js
@@ -0,0 +1,45 @@
+// This document uses globals and other nasty JS shit but that's ok
+// because i'm keeping it as small as reasonably possible without golfing.
+
+// Defines the colors for the dark and light theme
+lightTheme = [
+    ["--background", "#d5d6db"],
+    ["--text-color", "#343b58"],
+    ["--accent", "#5a4a78"],
+    ["--pale", "#cfc9c2"],
+    ["--selection", "#a9b1d6"]
+];
+darkTheme = [
+    ["--background", "#1a1b26"],
+    ["--text-color", "#cfc9c2"],
+    ["--accent", "#bb9af7"],
+    ["--pale", "#cfc9c2"],
+    ["--selection", "#a9b1d6"]
+];
+
+// Get a handle on the document styles.
+// Colors are set in styles.css then changed here
+let style = document.documentElement.style;
+let lights = document.getElementById("lights");
+
+
+// When you click the button, change the theme
+lights.addEventListener("click", () => {
+    // If its currently the light theme
+    if (style.getPropertyValue("--background") == lightTheme[0][1]) {
+        // Change to the dark one
+        switchThemes(darkTheme);
+        // update the text
+        lights.textContent = "lights off";
+    } else {
+        // Same here
+        switchThemes(lightTheme);
+        lights.textContent = "lights on";
+    }
+})
+
+function switchThemes(theme) {
+    theme.forEach(pair => {
+        style.setProperty(pair[0], pair[1]);
+    });
+}
